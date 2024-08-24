7de37e291793a4531b4a73c18d22f9d79555391a
vutran1710 <me@vutr.io>
2024-08-24 13:22:15 +0700

    update

--- migrations/tier_1.sql
+++ migrations/tier_1.sql
@@ -6,6 +6,11 @@ CREATE TABLE transactions (
     "from" VARCHAR NOT NULL,
     "to" VARCHAR NOT NULL,
     value BIGINT NOT NULL,
-    timestamp TIMESTAMP NOT NULL,
-    range_index BIGINT PRIMARY KEY
+    timestamp TIMESTAMP NOT NULL
 );
+
+CREATE INDEX transactions_block_number_idx ON transactions (block_number);
+CREATE INDEX transactions_tx_index_idx ON transactions (tx_index);
+CREATE INDEX transactions_from_idx ON transactions ("from");
+CREATE INDEX transactions_to_idx ON transactions ("to");
+CREATE INDEX transactions_timestamp_idx ON transactions (timestamp);
--- migrations/tier_2.sql
+++ migrations/tier_2.sql
@@ -2,6 +2,8 @@ CREATE TABLE buy_sell (
     "user" VARCHAR NOT NULL,
     amount BIGINT NOT NULL,
     timestamp TIMESTAMP NOT NULL,
-    range_index BIGINT NOT NULL,
-    PRIMARY KEY ("user", range_index)
+    PRIMARY KEY ("user", timestamp)
 );
+
+CREATE INDEX buy_sell_user_idx ON buy_sell ("user");
+CREATE INDEX buy_sell_timestamp_idx ON buy_sell (timestamp);
